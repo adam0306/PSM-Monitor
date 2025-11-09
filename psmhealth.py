380705b1697734b7c3fad78500597b18f3ba6871
adam0306 <adamchandler0306@gmail.com>
2025-11-08 18:23:16 -0600

    let's pick this back up

--- psmhealth.py
+++ psmhealth.py
@@ -9,7 +9,7 @@ failures = {}
 # Replace "git" with the url of a raw list of FQDN servers that you would like to be monitored.
 psmservers = filter(None, requests.get('git').text.split('\n'))
 for psm in psmservers:
-    r = requests.get('https://' + psm + './psm/api/health', verify=False)
+    r = requests.get('https://' + psm + './psm/api/health', verify=true)
     if r.text != 'PASS':
         failures.update({psm: r.text})
 # Uncomment the line below if you would like for the failure messages to be printed to the screen.
